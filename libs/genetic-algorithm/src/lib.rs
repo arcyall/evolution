6c8bb605819a7958c1007e631f31ccc60e6dbd93
arcyall <105183977+arcyall@users.noreply.github.com>
2023-03-13 05:15:07 +0100

    implement chromosome

--- libs/genetic-algorithm/src/lib.rs
+++ libs/genetic-algorithm/src/lib.rs
@@ -1,4 +1,7 @@
+#![feature(type_alias_impl_trait)]
+
 use rand::{seq::SliceRandom, RngCore};
+use std::ops::Index;
 
 pub struct GeneticAlgorithm<S> {
     selection_method: S,
@@ -6,6 +9,11 @@ pub struct GeneticAlgorithm<S> {
 
 #[derive(Default)]
 pub struct RouletteSelection;
+
+#[derive(Clone, Debug)]
+pub struct Chromosome {
+    genes: Vec<f32>,
+}
 
 pub trait Individual {
     fn fitness(&self) -> f32;
@@ -15,6 +23,46 @@ pub trait SelectionMethod {
     fn select<'a, T>(&self, rng: &mut dyn RngCore, population: &'a [T]) -> &'a T
     where
         T: Individual;
+}
+
+#[allow(clippy::len_without_is_empty)]
+impl Chromosome {
+    pub fn len(&self) -> usize {
+        self.genes.len()
+    }
+
+    pub fn iter(&self) -> impl Iterator<Item = &f32> {
+        self.genes.iter()
+    }
+
+    pub fn iter_mut(&mut self) -> impl Iterator<Item = &mut f32> {
+        self.genes.iter_mut()
+    }
+}
+
+impl Index<usize> for Chromosome {
+    type Output = f32;
+
+    fn index(&self, index: usize) -> &Self::Output {
+        &self.genes[index]
+    }
+}
+
+impl FromIterator<f32> for Chromosome {
+    fn from_iter<T: IntoIterator<Item = f32>>(iter: T) -> Self {
+        Self {
+            genes: iter.into_iter().collect(),
+        }
+    }
+}
+
+impl IntoIterator for Chromosome {
+    type Item = f32;
+    type IntoIter = impl Iterator<Item = f32>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.genes.into_iter()
+    }
 }
 
 impl<S> GeneticAlgorithm<S>
