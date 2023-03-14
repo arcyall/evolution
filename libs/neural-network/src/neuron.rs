d5b4e35cd60a2af328981796b8f8ec0d83876003
arcyall <105183977+arcyall@users.noreply.github.com>
2023-03-15 00:32:57 +0100

    extract neural network structs into seperate files

---
+++ libs/neural-network/src/neuron.rs
@@ -0,0 +1,72 @@
+use rand::Rng;
+
+#[derive(Debug)]
+pub(crate) struct Neuron {
+    pub(crate) bias: f32,
+    pub(crate) weights: Vec<f32>,
+}
+
+impl Neuron {
+    pub(crate) fn propagate(&self, inputs: &[f32]) -> f32 {
+        assert_eq!(inputs.len(), self.weights.len());
+
+        (inputs
+            .iter()
+            .zip(&self.weights)
+            .map(|(input, weight)| input * weight)
+            .sum::<f32>()
+            + self.bias)
+            .max(0.0)
+    }
+
+    pub fn random(rng: &mut dyn rand::RngCore, output_size: usize) -> Self {
+        Self {
+            bias: rng.gen_range(-1.0..=1.0),
+            weights: (0..output_size)
+                .map(|_| rng.gen_range(-1.0..=1.0))
+                .collect(),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use approx::assert_relative_eq;
+
+    mod random {
+        use super::*;
+        use rand::SeedableRng;
+        use rand_chacha::ChaCha8Rng;
+
+        #[test]
+        fn test() {
+            let mut rng = ChaCha8Rng::from_seed(Default::default());
+            let neuron = Neuron::random(&mut rng, 4);
+
+            assert_relative_eq!(neuron.bias, -0.6255188);
+            assert_relative_eq!(
+                neuron.weights.as_slice(),
+                [0.67383957, 0.8181262, 0.26284897, 0.5238807].as_ref()
+            );
+        }
+    }
+
+    mod propagate {
+        use super::*;
+
+        #[test]
+        fn test() {
+            let neuron = Neuron {
+                bias: 0.5,
+                weights: vec![-0.3, 0.8],
+            };
+
+            assert_relative_eq!(neuron.propagate(&[-10.0, -10.0]), 0.0);
+            assert_relative_eq!(
+                neuron.propagate(&[0.5, 1.0]),
+                (-0.3 * 0.5) + (0.8 * 1.0) + 0.5
+            );
+        }
+    }
+}
