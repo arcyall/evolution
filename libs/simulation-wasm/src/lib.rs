8626c1c60306539f0c80be419e881fc43b542295
arcyall <105183977+arcyall@users.noreply.github.com>
2023-03-17 00:11:40 +0100

    create wasm wrapper for simulation

--- libs/simulation-wasm/src/lib.rs
+++ libs/simulation-wasm/src/lib.rs
@@ -1,7 +1,54 @@
+use lib_simulation as sim;
+use rand::{rngs::ThreadRng, thread_rng};
+use serde::Serialize;
 use wasm_bindgen::prelude::*;
 
+#[wasm_bindgen]
+pub struct Simulation {
+    rng: ThreadRng,
+    sim: sim::Simulation,
+}
 
 #[wasm_bindgen]
-pub fn whos_that_dog() -> String {
-    "john".into()
+impl Simulation {
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> Self {
+        let mut rng = thread_rng();
+        let sim = sim::Simulation::random(&mut rng);
+
+        Self { rng, sim }
+    }
+
+    pub fn world(&self) -> JsValue {
+        let world = World::from(self.sim.world());
+        serde_wasm_bindgen::to_value(&world).unwrap()
+    }
+}
+
+#[derive(Clone, Debug, Serialize)]
+pub struct World {
+    pub animals: Vec<Animal>,
+}
+
+impl From<&sim::World> for World {
+    fn from(world: &sim::World) -> Self {
+        Self {
+            animals: world.animals().iter().map(Animal::from).collect(),
+        }
+    }
+}
+
+#[derive(Clone, Debug, Serialize)]
+pub struct Animal {
+    pub x: f32,
+    pub y: f32,
+}
+
+impl From<&sim::Animal> for Animal {
+    fn from(animal: &sim::Animal) -> Self {
+        Self {
+            x: animal.position().x,
+            y: animal.position().y,
+        }
+    }
 }
