72eb584ab92d8851f49d8ca59d4d17ca639195b9
arcyall <105183977+arcyall@users.noreply.github.com>
2023-04-29 04:57:13 +0200

    implement depth buffer

--- libs/simulation-wgpu/src/lib.rs
+++ libs/simulation-wgpu/src/lib.rs
@@ -114,6 +114,7 @@ struct State {
     instances: Vec<Instance>,
     instance_buffer: wgpu::Buffer,
     num_indices: u32,
+    depth_texture: Texture,
     // diffuse_bind_group: wgpu::BindGroup,
 }
 
@@ -242,6 +243,9 @@ impl State {
             usage: wgpu::BufferUsages::VERTEX,
         });
 
+        let depth_texture =
+            texture::Texture::create_depth_texture(&device, &config, "Depth Texture");
+
         let shader = device.create_shader_module(wgpu::include_wgsl!("shader.wgsl"));
 
         let render_pipeline_layout =
@@ -277,7 +281,13 @@ impl State {
                 polygon_mode: wgpu::PolygonMode::Fill,
                 conservative: false,
             },
-            depth_stencil: None,
+            depth_stencil: Some(wgpu::DepthStencilState {
+                format: texture::Texture::DEPTH_FORMAT,
+                depth_write_enabled: true,
+                depth_compare: wgpu::CompareFunction::Less,
+                stencil: wgpu::StencilState::default(),
+                bias: wgpu::DepthBiasState::default(),
+            }),
             multisample: wgpu::MultisampleState {
                 count: 1,
                 mask: !0,
@@ -342,6 +352,7 @@ impl State {
             num_indices,
             instances,
             instance_buffer,
+            depth_texture,
             // diffuse_bind_group,
         }
     }
@@ -355,6 +366,8 @@ impl State {
             self.size = new_size;
             self.config.height = new_size.height;
             self.config.width = new_size.width;
+            self.depth_texture =
+                texture::Texture::create_depth_texture(&self.device, &self.config, "Depth Texture");
             self.surface.configure(&self.device, &self.config)
         }
     }
@@ -400,7 +413,14 @@ impl State {
                         store: true,
                     },
                 })],
-                depth_stencil_attachment: None,
+                depth_stencil_attachment: Some(wgpu::RenderPassDepthStencilAttachment {
+                    view: &self.depth_texture.view,
+                    depth_ops: Some(wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(1.0),
+                        store: true,
+                    }),
+                    stencil_ops: None,
+                }),
             });
 
             render_pass.set_pipeline(&self.render_pipeline);
@@ -471,7 +491,6 @@ const VERTICES: &[Vertex] = &[
 ];
 
 const INDICES: &[u16] = &[0, 1, 4, 1, 2, 4, 2, 3, 4];
-
 const INSTANCES_PER_ROW: u32 = 10;
 const INSTANCE_DISPLACEMENT: Vector3<f32> = Vector3::new(
     INSTANCES_PER_ROW as f32 * 0.5,
