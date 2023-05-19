0965d4a9c90d614fbef169e4b8e5dd53698263ad
arcyall <105183977+arcyall@users.noreply.github.com>
2023-05-19 05:57:52 +0200

    swap present mode to fifo

--- libs/simulation-wgpu/src/lib.rs
+++ libs/simulation-wgpu/src/lib.rs
@@ -179,7 +179,7 @@ impl State {
             format: surface_format,
             width: size.width,
             height: size.height,
-            present_mode: surface_caps.present_modes[0],
+            present_mode: wgpu::PresentMode::Fifo,
             alpha_mode: surface_caps.alpha_modes[0],
             view_formats: vec![],
         };
